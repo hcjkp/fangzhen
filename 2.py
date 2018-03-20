44a129ac1d72a60c20e67028b0ae4f354f49aaf6
ztg <1094362935@qq.com>
2018-03-20 16:13:41 +0800

    company

--- 2.py
+++ 2.py
@@ -2,9 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from load import Conditioner
+a=np.mat([[-5,0],[0.1,-0.1]])
+b=np.mat([[5],[0]])
+p0=1000
+y2=[np.mat([[p0],[p0]])]*1000
+
+c=[0]*1000
+h=1
 
 
-y=np.array([2]*4)
-y1=np.array([1]*4)
-x=y-y1
-print(x[1])
+
+
+
