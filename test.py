0b5e26395131ed871c164333023bd8df4a172122
ztg <1094362935@qq.com>
2018-03-22 11:31:32 +0800

    company

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def fxy(x,y):
-   out=np.dot(a, y) + np.dot(b, np.array([31, 95, 70, 1200*x, 23]))
-   return out
+
+
+def fxy(x, y):
+    out = np.dot(a, y) + np.dot(b, np.array([31, 95, 70, 1200 * x, 23]))
+    return out
+
 
 for i in range(10):
     c_ew = np.random.normal(1.875, 0.1875) * 100000  # 外墙的热容量
@@ -15,7 +18,9 @@ for i in range(10):
 
     x_ext = np.random.uniform(31, 32)  # 外界温度
     x_adj = np.random.uniform(23, 30)  # 相邻房间的温度
-    i_eq = np.random.uniform(45, 55) + np.random.uniform(30, 35)  # 太阳通过玻璃表面以及室内家具的辐射量
+    i_eq = np.random.uniform(45,
+                             55) + np.random.uniform(30,
+                                                     35)  # 太阳通过玻璃表面以及室内家具的辐射量
     i_ew = np.random.uniform(95, 105)  # 太阳通过外墙表面的辐射量
     a = np.array(
         [[(1 / c_ew) * ((r_cew / (r_ew * (r_ew + r_cew)) - (2 / r_ew))), 1 / (c_ew * r_ew), 0],
@@ -27,7 +32,6 @@ for i in range(10):
         [[(1 / c_ew) * (1 / (r_cew + r_ew)), r_cew / (c_ew * (r_ew + r_cew)), 0, 0, 0],
          [1 / (c_in * r_gs), 0, 1 / c_in, -1 / c_in, 0],
          [0, 0, 0, 0, 1 / (c_iw * r_iw)]])
-
 
     def fun(t1, t2, t0):
         y = [np.array([30, t0, 24])]
@@ -44,13 +48,13 @@ for i in range(10):
             else:
                 w[i + 1] = w[i]
             y.append(y[i] + fxy(w[i], y[i]) * h)
-            y[i + 1] = y[i] + (fxy(w[i + 1], y[i + 1]) + fxy(w[i], y[i])) * (h / 2)
+            y[i + 1] = y[i] + (fxy(w[i + 1], y[i + 1]) +
+                               fxy(w[i], y[i])) * (h / 2)
             out[i + 1] = y[i + 1][1]
         print(y)
         plt.plot(t, out)
         plt.show()
 
-
     fun(22, 23, 22.5)
 
-    print(c_ew,i_ew,c_iw)
+    print(c_ew, i_ew, c_iw)
--- yuan.py
+++ yuan.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import math
 from load import Conditioner
 
-def fangc(a,b,y,c,h):
+
+def fangc(a, b, y, c, h):
     out = y + (a * y + b * c) * h
     out2 = y + (a * y + b * c + b * c + a * out) * h / 2
     return out2
 
-def lici(uref,n,h): #ut为标幺值
+
+def lici(uref, n, h):  # ut为标幺值
     a = np.mat([[-50, 0, 0],
                 [249.6, -250.2667, -1.25],
                 [6240, -6240, -31.25]])
@@ -19,18 +21,23 @@ def lici(uref,n,h): #ut为标幺值
     for i in range(10000):
         ut[i + 1] = ut[i] + h * (y[i][2] / 5.36 - ut[i] / 5.36 / 0.8)
         c = uref - ut[i]
-        y[i+1] = y[i] + (a * y[i] + b * c) * h
+        y[i + 1] = y[i] + (a * y[i] + b * c) * h
         y[i + 1] = y[i] + (a * y[i] + b * c + b * c + a * y[i + 1]) * h / 2
     return y
-print(lici(1,1000,0.001))
 
-def jiefangcheng(a,b,y,c,n,h):  #a，b 数组, y,c 可为列表，a为 n行 n列，b为n行 m列 c为m行
-    for i in range(n-1):
-        y[i+1]=y[i]+(a*y[i]+b*c[i])*h
-        y[i+1]=y[i]+(a*y[i+1]+b*c[i+1]+a*y[i]+b*c[i])*h/2
+
+print(lici(1, 1000, 0.001))
+
+
+def jiefangcheng(a, b, y, c, n, h):  # a，b 数组, y,c 可为列表，a为 n行 n列，b为n行 m列 c为m行
+    for i in range(n - 1):
+        y[i + 1] = y[i] + (a * y[i] + b * c[i]) * h
+        y[i + 1] = y[i] + (a * y[i + 1] + b * c[i + 1] +
+                           a * y[i] + b * c[i]) * h / 2
     return y
 
-def yuandong(p0,betaw,n):
+
+def yuandong(p0, betaw, n):
     c = np.array([p0] * n) - np.array(betaw)
     y1 = [np.mat(p0)] * n
     y2 = [np.mat([[p0], [p0]])] * n
